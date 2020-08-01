29c7ce03ec91be91b90423b03e5a300fd0e0c00c
Robert Wang <rwang5688@outlook.com>
2020-08-01 04:27:37 -0700

    add sqs utility functions; rename test files

--- awsutil/s3util_test.py
+++ awsutil/s3util_test.py
@@ -8,18 +8,16 @@ def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('bucket_name', help='The name of the bucket to create.')
-    parser.add_argument('region', help='The region in which to create your bucket.')
+    parser.add_argument('bucket_name', help='The name of the bucket to upload file.')
     parser.add_argument('file_name', help='The name of the file to upload')
 
     args = parser.parse_args()
 
     bucket_name = args.bucket_name
-    region = args.region
     file_name = args.file_name
 
+    print('\nargs:')
     print(f'bucket_name = {bucket_name}')
-    print(f'region = {region}')
     print(f'file_name = {file_name}')
 
     s3util.list_buckets()
---
+++ awsutil/sqsutil_test.py
@@ -0,0 +1,48 @@
+import sys
+import boto3
+from botocore.exceptions import ClientError
+import sqsutil
+import jsonutil
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('queue_name', help='The name of the queue to send.')
+    parser.add_argument('file_name', help='The name of the message file to send and receive')
+
+    args = parser.parse_args()
+
+    queue_name = args.queue_name
+    file_name = args.file_name
+
+    print('\nargs:')
+    print(f'queue_name = {queue_name}')
+    print(f'file_name = {file_name}')
+
+    sqsutil.list_queues()
+    queue_url = sqsutil.get_queue_url(queue_name)
+    if queue_url is None:
+        print(f'\nQueue {queue_name} does not exist. Exit.')
+        return
+
+    message_body = jsonutil.get_json_data(file_name)
+    message_body_pretty_string = jsonutil.get_pretty_string(message_body)
+    message_body_string = jsonutil.get_string(message_body)
+    message_id = sqsutil.send_message(queue_url, message_body_string)
+    print('\nMessageBody: (in pretty print)')
+    print(message_body_pretty_string)
+    print(f'MessageId: {message_id}')
+
+    message = sqsutil.receive_message(queue_url)
+    if message is not None:
+        receipt_handle = message['ReceiptHandle']
+        sqsutil.delete_message(queue_url, receipt_handle)
+    print('\nReceived and deleted message:')
+    print(jsonutil.get_pretty_string(message))
+
+
+if __name__ == '__main__':
+    main()
+
