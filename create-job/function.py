42c395f19ab3bdbfecbb7de3bf637f3b32664fc8
Robert Wang <rwang5688@outlook.com>
2020-08-02 03:22:20 -0700

    annotate python lambda function shell

--- create-job/function.py
+++ create-job/function.py
@@ -17,25 +17,32 @@ logger.setLevel(logging.INFO)
 patch_all()
 
 
+# handler
 def createJob(event, context):
-    client = boto3.client('lambda')
     logger.info('## ENVIRONMENT VARIABLES\r' + jsonpickle.encode(dict(**os.environ)))
     logger.info('## EVENT\r' + jsonpickle.encode(event))
     logger.info('## CONTEXT\r' + jsonpickle.encode(context))
+    client = boto3.client('lambda')
     response = client.get_account_settings()
     return response['AccountUsage']
 
 
+# main function for testing and debugging handler
 def main():
     xray_recorder.begin_segment('main_function')
     file = open('event.json', 'rb')
     try:
+      # read sample event
       ba = bytearray(file.read())
       event = jsonpickle.decode(ba)
       logger.warning('## EVENT')
       logger.warning(jsonpickle.encode(event))
+      # create sample context
       context = {'requestid' : '1234'}
+      # invoke handler
       result = createJob(event, context)
+      # print response
+      print('## RESPONSE')
       print(str(result))
     finally:
       file.close()
