81a0bccdcda111c64f9fe3e5ad192e93ebd5515c
Robert Wang <rwang5688@outlook.com>
2020-08-07 02:49:55 -0700

    separate parse_event_record from create_job_record

--- updateJob/updateJob.py
+++ updateJob/updateJob.py
@@ -26,33 +26,70 @@ def preamble(event, context):
     return True
 
 
+def parse_event_record(event_record):
+    global job_id
+    global job_status
+    global job_logfile
+
+    event_body = eval(event_record['body'])
+    if event_body is None:
+        print('parse_event: event body is missing.')
+        return False
+
+    job = event_body['job']
+    if job is None:
+        print('parse_event: job is missing.')
+        return False
+
+    job_id = job['job_id']
+    if job_id is None:
+        print('parse_message: job id is missing.')
+        return False
+
+    job_status = job['job_status']
+    if job_status is None:
+        print('parse_message: job status is missing.')
+        return False
+
+    job_logfile = job['job_logfile']
+    if job_logfile is None:
+        print('parse_message: job logfile is missing.')
+        return False
+
+    # success
+    return True
+
+
 def send_message(queue_name, item):
     # get queue url
     sqsutil.list_queues()
     queue_url = sqsutil.get_queue_url(queue_name)
     if queue_url is None:
-        print(f'\nQueue {queue_name} does not exist.')
+        print(f'send_message: Queue {queue_name} does not exist.')
         return False
 
     # send message
     message_body = {
-        "action": "process",
+        "action": "update_log_stream",
         "job": {
-            "id": item['id'],
-            "tool": item['tool'],
-            "source": item['source']
+            "job_id": item['job_id'],
+            "job_status": item['job_status'],
+            "job_logfile": item['job_logfile']
         }
     }
     message_id = sqsutil.send_message(queue_url, str(message_body))
     print(f'MessageId: {message_id}')
     print(f'MessageBody: {message_body}')
 
-    # receive message
+    # debug: receive message
     message = sqsutil.receive_message(queue_url)
-    print('\nReceived message:')
+    if message is None:
+        print(f'send_message: cannot retrieve sent messge.')
+        return False
+    print('Received message:')
     print(message)
 
-    # successfully sent and received message
+    # success
     return True
 
 
@@ -66,39 +103,54 @@ def updateJob(event, context):
     # get jobs table
     jobs_table = jobstable.get_jobs_table()
     if jobs_table is None:
+        print('get_jobs_table failed.  Exit.')
         return False
 
     # create jobs record
     event_records = event['Records']
     for event_record in event_records:
-        print('\nEvent Record:')
+        # debug: print event record
+        print('Event record:')
         print(event_record)
 
-        # create job record
-        job_id = jobstable.create_job_record(jobs_table, event_record)
-        if job_id is None:
-            print('create_job_record failed.  Exit')
-            return False
-        item = jobstable.get_job_record(jobs_table, job_id)
-        if item is None:
-            print('get_job_record failed.  Exit')
-            return False
-        print('\nCreated Job Record:')
-        print(item)
+        # parse event record
+        success = parse_event_record(event_record)
+        if not success:
+            print('parse_event_record failed.  Exit.')
+            continue
 
-        # if environment variable exists, use it
-        queue_name = 'jobs-list-process-job-queue-rwang5688'
-        if 'PROCESS_JOB_QUEUE' in os.environ:
-            queue_name = os.environ['PROCESS_JOB_QUEUE']
-        send_message(queue_name, item)
-
-        # TO DO:
-        # Start ECS task!!!
+        # debug: print job record attributes
+        print('Job record attributes:')
+        print(f'job_id: {job_id}')
+        print(f'job_status: {job_status}')
+        print(f'job_logfile: {job_logfile}')
 
         # update job status
-        success = jobstable.update_job_status(jobs_table, job_id, "started", "")
+        success = jobstable.update_job_status(jobs_table, job_id, job_status, job_logfile)
+        if not success:
+            print('update_job_status failed.  Next.')
+            continue
 
-    # successfully processed all records
+        # debug: get and print job record
+        item = jobstable.get_job_record(jobs_table, job_id)
+        if item is None:
+            print('get_job_record failed.  Next.')
+            continue
+        print('Job record:')
+        print(item)
+
+        # set update jobs log stream queue name
+        queue_name = 'jobs-list-update-jobs-log-stream-queue-rwang5688'
+        if 'UPDATE_JOBS_LOG_STREAM_QUEUE' in os.environ:
+            queue_name = os.environ['UPDATE_JOBS_LOG_STREAM_QUEUE']
+
+        # send job context to update jobs log stream queue
+        success = send_message(queue_name, item)
+        if not success:
+            print('send_message failed.  Next.')
+            continue
+
+    # success
     return True
 
 
