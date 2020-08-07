9f5811c33e3e2ad17d901549e8b87902464efb23
Robert Wang <rwang5688@outlook.com>
2020-08-06 23:32:00 -0700

    use attributes with job_ prefix

--- createJob/createJob.py
+++ createJob/createJob.py
@@ -31,28 +31,31 @@ def send_message(queue_name, item):
     sqsutil.list_queues()
     queue_url = sqsutil.get_queue_url(queue_name)
     if queue_url is None:
-        print(f'\nQueue {queue_name} does not exist.')
+        print(f'send_message: Queue {queue_name} does not exist.')
         return False
 
     # send message
     message_body = {
         "action": "process",
         "job": {
-            "id": item['id'],
-            "tool": item['tool'],
-            "source": item['source']
+            "job_id": item['job_id'],
+            "job_tool": item['job_tool'],
+            "job_source": item['job_source']
         }
     }
     message_id = sqsutil.send_message(queue_url, str(message_body))
     print(f'MessageId: {message_id}')
     print(f'MessageBody: {message_body}')
 
-    # receive message
+    # debug: receive message
     message = sqsutil.receive_message(queue_url)
-    print('\nReceived message:')
+    if message is None:
+        print(f'send_message: cannot retrieve sent messge.')
+        return False
+    print('Received message:')
     print(message)
 
-    # successfully sent and received message
+    # success
     return True
 
 
@@ -66,12 +69,14 @@ def createJob(event, context):
     # get jobs table
     jobs_table = jobstable.get_jobs_table()
     if jobs_table is None:
+        print('get_jobs_table failed.  Exit.')
         return False
 
     # create jobs record
     event_records = event['Records']
     for event_record in event_records:
-        print('\nEvent Record:')
+        # debug: print event record
+        print('Event Record:')
         print(event_record)
 
         # create job record
@@ -79,24 +84,33 @@ def createJob(event, context):
         if job_id is None:
             print('create_job_record failed.  Exit')
             return False
+
+        # debug: get and print job record
         item = jobstable.get_job_record(jobs_table, job_id)
         if item is None:
             print('get_job_record failed.  Exit')
             return False
-        print('\nCreated Job Record:')
+        print('Job Record:')
         print(item)
 
-        # if environment variable exists, use it
+        # set process job queue name
         queue_name = 'jobs-list-process-job-queue-rwang5688'
         if 'PROCESS_JOB_QUEUE' in os.environ:
             queue_name = os.environ['PROCESS_JOB_QUEUE']
-        send_message(queue_name, item)
+
+        # send job context to process job queue
+        success = send_message(queue_name, item)
+        if not success:
+            print('send_message failed.  Exit.')
+            return False
 
         # TO DO:
-        # Start ECS task!!!
+        # Start ECS task to process job!!!
 
         # update job status
-        success = jobstable.update_job_status(jobs_table, job_id, "started", "")
+        job_status = "started"
+        job_logfile = ""
+        success = jobstable.update_job_status(jobs_table, job_id, job_status, job_logfile)
 
     # successfully processed all records
     return True
