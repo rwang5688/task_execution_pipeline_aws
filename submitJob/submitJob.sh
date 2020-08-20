3bc03f6a90223e59539a695654a4b8dc78dc0110
Robert Wang <rwang5688@outlook.com>
2020-08-20 01:58:04 -0700

    remove Dockerfile and move aws configure in script

--- submitJob/submitJob.sh
+++ submitJob/submitJob.sh
@@ -2,6 +2,10 @@
 # set env vars
 . ./env.sh
 
+# import AWS credentials
+aws configure import --csv "file://aws-admin_accessKeys.csv"
+cat ~/.aws/credentials
+
 # execute script
 python3 ./submitJob.py $1 $2
 
