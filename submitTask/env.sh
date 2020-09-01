f22b58e11636f3c6cfa6e3d3d0ccef17a980bc76
Robert Wang <rwang5688@outlook.com>
2020-09-01 01:20:15 -0700

    add result data bucket

--- submitTask/env.sh
+++ submitTask/env.sh
@@ -6,6 +6,7 @@ export TARGET_REGION=us-west-2
 # task-list workflow and database specific environment variables
 export TASK_LIST_SOURCE_DATA_BUCKET=task-list-source-data-bucket-rwang5688
 export TASK_LIST_LOG_DATA_BUCKET=task-list-log-data-bucket-rwang5688
+export TASK_LIST_RESULT_DATA_BUCKET=task-list-result-data-bucket-rwang5688
 export TASK_LIST_SUBMIT_TASK_QUEUE=task-list-submit-task-queue-rwang5688
 export TASK_LIST_PROCESS_TASK_QUEUE=task-list-process-task-queue-rwang5688
 export TASK_LIST_UPDATE_TASK_QUEUE=task-list-update-task-queue-rwang5688
