12d488ec06e8fae8a35a41859cf4a1578f674bf0
Robert Wang <rwang5688@outlook.com>
2020-09-03 03:08:14 -0700

    change processTask for real XVSA part 3

--- processTask/processTask.sh
+++ processTask/processTask.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# set env vars
+# set env vars and PATH
 . ./env.sh
+export PATH=$PATH:$PWD/bin
 
 # retrieve task, set task_id, set task_extra_options and download files
 echo "[CMD] python3 processTask.py"
 python3 processTask.py
-
-# print env vars
-#echo "SCAN_TASK_ID: ${SCAN_TASK_ID}"
-#echo "SCAN_EXTRA_OPTIONS: ${SCAN_EXTRA_OPTIONS}"
-#echo "SCAN_EXTRA_JFE_OPTIONS: ${SCAN_EXTRA_JFE_OPTIONS}"
-#echo "SCAN_EXTRA_VARIABLE_OPTION: ${SCAN_EXTRA_VARIABLE_OPTION}"
-#echo "SCAN_EXTRA_SKIP_VTABLE_OPTION: ${SCAN_EXTRA_VTABLE_OPTION}"
 
