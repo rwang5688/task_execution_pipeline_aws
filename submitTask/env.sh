02d2cb4e504d53df44142969d2a6c53a20a87993
Robert Wang <rwang5688@outlook.com>
2020-09-17 03:09:26 -0700

    change resource names and add issue table

--- submitTask/env.sh
+++ submitTask/env.sh
@@ -5,12 +5,13 @@ export TARGET_CLOUD=aws
 export TARGET_REGION=us-west-2
 
 # task-list workflow and database specific environment variables
-export TASK_LIST_PREPROCESS_DATA_BUCKET=task-list-preprocess-data-bucket-rwang5688
-export TASK_LIST_LOG_DATA_BUCKET=task-list-log-data-bucket-rwang5688
-export TASK_LIST_RESULT_DATA_BUCKET=task-list-result-data-bucket-rwang5688
-export TASK_LIST_SUBMIT_TASK_QUEUE=task-list-submit-task-queue-rwang5688
-export TASK_LIST_PROCESS_TASK_QUEUE=task-list-process-task-queue-rwang5688
-export TASK_LIST_UPDATE_TASK_QUEUE=task-list-update-task-queue-rwang5688
-export TASK_LIST_UPDATE_TASK_LOG_STREAM_QUEUE=task-list-update-task-log-stream-queue-rwang5688
-export TASK_LIST_TASK_TABLE=task-list-task-table-rwang5688
+export TASK_EXEC_PREPROCESS_DATA_BUCKET=task-exec-preprocess-data-bucket-rwang5688
+export TASK_EXEC_LOG_DATA_BUCKET=task-exec-log-data-bucket-rwang5688
+export TASK_EXEC_RESULT_DATA_BUCKET=task-exec-result-data-bucket-rwang5688
+export TASK_EXEC_SUBMIT_TASK_QUEUE=task-exec-submit-task-queue-rwang5688
+export TASK_EXEC_PROCESS_TASK_QUEUE=task-exec-process-task-queue-rwang5688
+export TASK_EXEC_UPDATE_TASK_QUEUE=task-exec-update-task-queue-rwang5688
+export TASK_EXEC_UPLOAD_TASK_ISSUES_QUEUE=task-exec-upload-task-issues-queue-rwang5688
+export TASK_EXEC_TASK_TABLE=task-exec-task-table-rwang5688
+export TASK_EXEC_ISSUE_TABLE=task-exec-issue-table-rwang5688
 
